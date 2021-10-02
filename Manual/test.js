f810682cb43fdb5a543ebf1cb06d3119a2c31250
peoplestealusernames <wintercar101@gmail.com>
2021-10-02 01:28:08 -0500

    removed unneeded files

--- Manual/test.js
+++
@@ -1,6 +0,0 @@
-const net = require('net');
-const client = net.connect({port: 80, host: "google.com"}, () =>
-{
-    console.log('MyIP=' + client.localAddress);
-    console.log('MyPORT=' + client.localPort);
-});
