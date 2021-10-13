8ce5f8f90eda3af2c8b51de6ab0df643ece57f2e
peoplestealusernames <wintercar101@gmail.com>
2021-10-13 01:10:41 -0500

    Updated package added TODO

--- index.ts
+++ index.ts
@@ -7,5 +7,5 @@ export * from './ClientAPI'
 export * from './ServerClass'
 
 //TODO: check function that will return needed vars for server or client
-//TODO: isSetup():bool thats automatic to be ref by other API
+//TODO: isSetup():bool thats automatic to be ref by other API (runs in test from index)
 //TODO: Set*varname*(value) for all needed to be phrased
