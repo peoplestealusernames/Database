8fd5b7cfc3deccb410f105b80fef4e118e1bc038
peoplestealusernames <wintercar101@gmail.com>
2021-10-21 19:52:12 -0500

    Renamed ServerClass to ServerAPI to be more inline with ClientAPI

--- tenk-database/index.ts
+++ tenk-database/index.ts
@@ -4,7 +4,7 @@
 export * from './DMClass'
 export * from './ConnectionClass'
 export * from './ClientAPI'
-export * from './ServerClass'
+export * from './ServerAPI'
 
 //TODO: check function that will return needed vars for server or client
 //TODO: isSetup():bool thats automatic to be ref by other API (runs in test from index)
