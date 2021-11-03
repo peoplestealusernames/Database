75e85040f28b0406e36c2ffa33b4adba03124354
peoplestealusernames <wintercar101@gmail.com>
2021-11-03 17:53:46 -0500

    Readded firebase FB put into serverDEV Added new selfTCP into server.ts (DEV)

--- ServerDev/server.ts
+++ ServerDev/server.ts
@@ -1,14 +1,26 @@
 import { Server, Connection, DataHandler, DataManger } from 'tenk-database/ts/index'
+import { connect } from 'net';
+import { LogIn, UpdateIP } from './FBPut';
 
 var Ser: Server
 
 start()
 async function start() {
-    Ser = new Server("localhost", 8000) //TODO: TESTMODE ONLY
-    Ser.on('client', (Client: Connection) => {
-        Client.on('data', (data: any) => { DataRec(data, Client) })
-        Client.on('error', console.log)
-    })
+    await LogIn()
+
+    const iptest = connect({ port: 80, host: "google.com" }, async () => {
+        const ip = iptest.localAddress
+        const port = iptest.localPort
+
+        console.log('Public ip: ', ip, port)
+        UpdateIP(ip, port)
+
+        Ser = new Server(ip, port) //TODO: TESTMODE ONLY
+        Ser.on('client', (Client: Connection) => {
+            Client.on('data', (data: any) => { DataRec(data, Client) })
+            Client.on('error', console.log)
+        })
+    });
 }
 
 /////////////////////////
