d7a3c28ea1bc2c697e5032f92c2edd9088e97cf6
peoplestealusernames <50470278+peoplestealusernames@users.noreply.github.com>
2022-02-25 21:20:41 -0600

    Cleint: Added tests

--- ClientDev/client.ts
+++ ClientDev/client.ts
@@ -17,4 +17,9 @@ const Server = new Client("localhost", 5555)
 
 Server.on('setup', () => {
     Server.get('RGB').then((data) => { console.log("AYE ", data) })
+    let x = 1000
+    setTimeout(_ => { Server.put('RGB', { r: 255, g: 255, b: 255 }) }, x); x += 1000
+    setTimeout(_ => { Server.put('A', 1) }, x); x += 1000
+    setTimeout(_ => { Server.put('C/B/A', "foobar") }, x); x += 1000
+    setTimeout(_ => { Server.put('aye', 'lmao') }, x); x += 1000
 })
